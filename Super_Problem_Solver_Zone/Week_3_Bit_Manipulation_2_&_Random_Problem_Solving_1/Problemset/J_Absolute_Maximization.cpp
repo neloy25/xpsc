6f78a38551e294d06c9c94b6ee78aaf276ae4b58
neloy25 <sadmansafinneloy678@gmail.com>
2026-02-14 15:37:33 +0600

    Week3 problemset J

---
+++ Super_Problem_Solver_Zone/Week_3_Bit_Manipulation_2_&_Random_Problem_Solving_1/Problemset/J_Absolute_Maximization.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl << '\n'
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    int x = -1, y = 0;
+
+    for(int i = 0; i < n; i++) {
+        x &= v[i];
+        y |= v[i];
+    }
+
+    cout << y - x nl;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
