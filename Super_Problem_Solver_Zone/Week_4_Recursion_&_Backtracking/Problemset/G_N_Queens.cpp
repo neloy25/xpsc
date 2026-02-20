bf47908fc8d412f8c19480f4d9733b03084064a9
neloy25 <sadmansafinneloy678@gmail.com>
2026-02-20 19:25:48 +0600

    Week 4 problemset g edited

--- Super_Problem_Solver_Zone/Week_4_Recursion_&_Backtracking/Problemset/G_N_Queens.cpp
+++ Super_Problem_Solver_Zone/Week_4_Recursion_&_Backtracking/Problemset/G_N_Queens.cpp
@@ -8,16 +8,8 @@ class Solution {
     vvs ans;
 
     bool isSafe(vs & board, int row, int col, int n) {
-        for(int i = 0; i < n; i++) {
-            if(board[row][i] == 'Q') return false;
-        }
-        
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < row; i++) {
             if(board[i][col] == 'Q') return false;
-        }
-
-        for(int i = row, j = col; i < n && j < n; i++, j++) {
-            if(board[i][j] == 'Q') return false;
         }
 
         for(int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
@@ -27,11 +19,6 @@ class Solution {
         for(int i = row, j = col; i >= 0 && j < n; i--, j++) {
             if(board[i][j] == 'Q') return false;
         }
-
-        for(int i = row, j = col; i < n && j >= 0; i++, j--) {
-            if(board[i][j] == 'Q') return false;
-        }
-
         return true;
     }
 
